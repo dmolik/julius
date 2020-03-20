4161a4345899f9d3b3a42b394a4d91243c566fea
Dan Molik <dan@danmolik.com>
2020-03-20 07:54:48 -0400

    Update schema to include users, and indexes

--- calendar.sql
+++ calendar.sql
@@ -1,5 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 DROP TABLE IF EXISTS calendar;
+DROP TABLE IF EXISTS users;
 /*CREATE TABLE vcal (
 	id uuid DEFAULT uuid_generate_v4(),
 	created timestamp,
@@ -17,9 +19,26 @@ DROP TABLE IF EXISTS calendar;
 
 CREATE TABLE calendar (
 	id uuid DEFAULT uuid_generate_v4(),
-	rpath text,
-	content text,
+	user_id INT NOT NULL,
+	modified TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	rpath   TEXT,
+	content TEXT,
 	parent uuid,
-	children uuid[],
-	modified TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+	children uuid[]
 );
+
+/* INSERT INTO users (username, password) VALUES ("dan", crypt("somepassword", gen_salt('bf'))); */
+CREATE TABLE users (
+	id         SERIAL PRIMARY KEY,
+	username   TEXT NOT NULL,
+	email      TEXT NOT NULL,
+	password   VARCHAR(64) NOT NULL, /* crypt('input', password) */
+	firstname  TEXT NOT NULL,
+	lastname   TEXT NOT NULL,
+	isverified BOOLEAN DEFAULT false
+);
+
+CREATE INDEX user_index ON users (username, password);
+CREATE INDEX user_cal_index ON users (id, firstname, lastname);
+CREATE INDEX cal_index ON calendar (rpath);
+CREATE INDEX cal_user_index ON calendar (rpath, user_id);
